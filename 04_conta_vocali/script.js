f0dd7c615730c6abeee13b5dc25fe027f8c89c0f
SimoneCriscuolo233 <simone.criscuolo26052001@gmail.com>
2025-07-08 15:07:50 +0200

    feat: created a function that accepts a string and returns the number of vowels contained within it

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,10 +4,31 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+const countVowels = (string) => {
+  let vowels = [];
+  for (let i = 0; i < string.length; i++) {
+    if ('aeiou'.includes(string[i].toLowerCase())) {
+      vowels.push(string[i].toLowerCase());
+    }
+  }
+  return `${vowels.length}, (${vowels.join(', ')})`;
+}
 
+/* 
+-----Funzione con nome-----
+function countVowels(string) {
+let vowels = [];
+  for (let i = 0; i < string.length; i++) {
+    if ('aeiou'.includes(string[i].toLowerCase())) {
+      vowels.push(string[i].toLowerCase());
+    }
+  }
+  return `${vowels.length}, (${vowels.join(', ')})`;
+}
+*/
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(countVowels(word));
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
