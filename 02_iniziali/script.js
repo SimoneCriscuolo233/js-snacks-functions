7b140c179d867188eb01b2e214800b1a89e4d969
SimoneCriscuolo233 <simone.criscuolo26052001@gmail.com>
2025-07-08 14:17:46 +0200

    feat: created a function that takes an array of names and returns a new array with the initials of each word in the supplied array

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -4,10 +4,28 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+const getInitials = (namesArray) => {
+  const initials = [];
+  for (let i = 0; i < namesArray.length; i++) {
+    initials.push(namesArray[i][0].toUpperCase());
+  }
+  return initials;
+};
 
+//-----Funzione con nome-----
+
+/*
+function getInitials(namesArray) {
+const initials = [];
+  for (let i = 0; i < namesArray.length; i++) {
+    initials.push(namesArray[i][0].toUpperCase());
+  }
+  return initials;
+};
+*/
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(getInitials(names));
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
