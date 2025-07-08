19b71449f228a53553890cf7afd3b033cc092776
SimoneCriscuolo233 <simone.criscuolo26052001@gmail.com>
2025-07-08 14:05:55 +0200

    feat: created function that greets the user

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -4,10 +4,18 @@ const userName = 'Mario';
 
 
 // Dichiara la funzione qui.
+const greetUser = name => `Ciao ${name}`;
 
+//-----Funzione con nome-----
+
+//function greetUser(name) {
+//  return `Ciao ${name}`;
+//}
 
 // Invoca la funzione qui e stampa il risultato in console
 
+greetUser(userName);
+console.log(greetUser(userName));
 
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
